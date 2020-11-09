ab4885629940dd19041674b38cb3248765d280a9
yogurt-shadow <1711143@mail.nankai.edu.cn>
2020-11-09 22:43:01 +0800

    update proj1a format

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,5 +1,5 @@
 public class ArrayDeque<T> {
-    
+
     private int size;
     private T[] iTs;
     private int nextfirst;
@@ -10,27 +10,29 @@ public class ArrayDeque<T> {
         iTs = (T[]) new Object[8];
         nextfirst = 0;
         nextlast = 1;
-    } 
-
-
-    
-    /**public ArrayDeque(ArrayDeque other) {
-        this.size = other.size;
-        this.iTs = (T[]) new Object[other.iTs.length];
-        System.arraycopy(other.iTs, 0, iTs, 0, other.iTs.length);
-        this.nextfirst = 0;
-        this.nextlast = 1;
     }
-    */
+
+
+    /**
+     * public ArrayDeque(ArrayDeque other) {
+     * this.size = other.size;
+     * this.iTs = (T[]) new Object[other.iTs.length];
+     * System.arraycopy(other.iTs, 0, iTs, 0, other.iTs.length);
+     * this.nextfirst = 0;
+     * this.nextlast = 1;
+     * }
+     */
 
     public int size() {
         return size;
-    };
+    }
+
+    ;
 
     private void resize(int x) {
         T[] home = (T[]) new Object[x];
         for (int i = 0; i < size; i++) {
-        	home[i] = get(i);
+            home[i] = get(i);
         }
         nextfirst = x - 1;
         nextlast = size;
@@ -38,126 +40,126 @@ public class ArrayDeque<T> {
     }
 
     private void upsize() {
-    	resize(size * 2);
+        resize(size * 2);
     }
 
     private void downsize() {
-    	resize(iTs.length / 2);
+        resize(iTs.length / 2);
     }
 
     private boolean isFull() {
-    	return size == iTs.length;
+        return size == iTs.length;
     }
 
 
-	public void addFirst(T i) {
-		if (isFull()) {
-			upsize();
-		}
-		iTs[nextfirst] = i;
-		if (nextfirst == 0) {
-			nextfirst = iTs.length - 1;
-		} else { 
-			nextfirst--; 
-		}
-		size += 1;
-	}
+    public void addFirst(T i) {
+        if (isFull()) {
+            upsize();
+        }
+        iTs[nextfirst] = i;
+        if (nextfirst == 0) {
+            nextfirst = iTs.length - 1;
+        } else {
+            nextfirst--;
+        }
+        size += 1;
+    }
 
-	public void addLast(T i) {
-		if (isFull()) {
-			upsize();
-		}
-		iTs[nextlast] = i;
-		if (nextlast == iTs.length - 1) {
-			nextlast = 0;
-		} else { 
-			nextlast++; 
-		}
-		size += 1;
+    public void addLast(T i) {
+        if (isFull()) {
+            upsize();
+        }
+        iTs[nextlast] = i;
+        if (nextlast == iTs.length - 1) {
+            nextlast = 0;
+        } else {
+            nextlast++;
+        }
+        size += 1;
     }
 
     public boolean isEmpty() {
-    	return size == 0;
+        return size == 0;
     }
 
     public void printDeque() {
-    	if (this.isEmpty()) {
-    		System.out.println("");
-    	} else {
-    		for (int i = 0; i < size; i++) {
-    			System.out.print(this.get(i) + " ");
-    		}
-    		System.out.println("");
-    	}
+        if (this.isEmpty()) {
+            System.out.println("");
+        } else {
+            for (int i = 0; i < size; i++) {
+                System.out.print(this.get(i) + " ");
+            }
+            System.out.println("");
+        }
     }
 
     public T removeFirst() {
-    	if (size * 4 < iTs.length  && iTs.length > 8) {
-    		downsize();
-    		if (size == 0) {
-    			return null;
-    		}
-    		size -= 1;
-    		if (nextfirst == iTs.length - 1) {
-    			nextfirst = 0;
-    		} else {
-    			nextfirst++;
-    		}
-    		T result = iTs[nextfirst];
-    		return result;
-    	} else {
-    		if (size == 0) {
-    			return null;
-    		}
-    		size -= 1;
-    		if (nextfirst == iTs.length - 1) {
-    			nextfirst = 0;
-    		} else {
-    			nextfirst++;
-    		}
-    		T result = iTs[nextfirst];
-    		iTs[nextfirst] = null;
-    		return result;
-    	}
+        if (size * 4 < iTs.length && iTs.length > 8) {
+            downsize();
+            if (size == 0) {
+                return null;
+            }
+            size -= 1;
+            if (nextfirst == iTs.length - 1) {
+                nextfirst = 0;
+            } else {
+                nextfirst++;
+            }
+            T result = iTs[nextfirst];
+            return result;
+        } else {
+            if (size == 0) {
+                return null;
+            }
+            size -= 1;
+            if (nextfirst == iTs.length - 1) {
+                nextfirst = 0;
+            } else {
+                nextfirst++;
+            }
+            T result = iTs[nextfirst];
+            iTs[nextfirst] = null;
+            return result;
+        }
     }
 
     public T removeLast() {
-    	if (size * 4 < iTs.length && iTs.length > 8) {
-    		downsize();
-    		if (size == 0) {
-    			return null;
-    		}
-    		size -= 1;
-    		if (nextlast == 0) {
-    			nextlast = iTs.length - 1;
-    		} else {
-    			nextlast--;
-    		}
-    		T result = iTs[nextlast];
-    		return result;
-    	} else {
-    		if (size == 0) {
-    			return null;
-    		}
-    		size -= 1;
-    		if (nextlast == 0) {
-    			nextlast = iTs.length - 1;
-    		} else {
-    			nextlast--;
-    		}
-    		T result = iTs[nextlast];
-    		iTs[nextlast] = null;
-    		return result;
-    	}
+        if (size * 4 < iTs.length && iTs.length > 8) {
+            downsize();
+            if (size == 0) {
+                return null;
+            }
+            size -= 1;
+            if (nextlast == 0) {
+                nextlast = iTs.length - 1;
+            } else {
+                nextlast--;
+            }
+            T result = iTs[nextlast];
+            return result;
+        } else {
+            if (size == 0) {
+                return null;
+            }
+            size -= 1;
+            if (nextlast == 0) {
+                nextlast = iTs.length - 1;
+            } else {
+                nextlast--;
+            }
+            T result = iTs[nextlast];
+            iTs[nextlast] = null;
+            return result;
+        }
     }
 
     public T get(int index) {
-    	return iTs[(nextfirst + 1 + index) % iTs.length];
+        return iTs[(nextfirst + 1 + index) % iTs.length];
     }
 
 /**
-    public void showlength(){
-    System.out.println(iTs.length);
-    }
-    */
+ public void showlength(){
+ System.out.println(iTs.length);
+ }
+ */
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,137 +1,138 @@
 public class LinkedListDeque<T> {
-    
+
     private IntNode first;
     private IntNode last;
     private int size;
 
     private class IntNode {
-	    
-	    private T item;
-	    private IntNode next；
-	    private IntNode prev;
 
-	    public IntNode(T i, IntNode n, IntNode p) {
-		    item = i;
-		    next = n;
-		    prev = p;
-	    }
+        private T item;
+        private IntNode next;
+        private IntNode prev;
+
+        public IntNode(T i, IntNode n, IntNode p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
     }
 
     public LinkedListDeque() {
-	    first = null;
-	    last = null;
-	    size = 0;
+        first = null;
+        last = null;
+        size = 0;
     }
 
 
     private LinkedListDeque copy() {
-	    LinkedListDeque<T> result = new LinkedListDeque<T>();
-	    for (int j = 0; j < this.size; j++) {
-		    result.addLast((T) this.get(j));
-	    }
-	    return result;
+        LinkedListDeque<T> result = new LinkedListDeque<T>();
+        for (int j = 0; j < this.size; j++) {
+            result.addLast((T) this.get(j));
+        }
+        return result;
     }
-
 
 
     public void addFirst(T x) {
-	    first = new IntNode(x, first, null);
-	    size += 1;
-	    if (first.next != null) {
-		    first.next.prev = first;
-	    }
-	    if (last == null) {
-		    last = first;
-	    }
+        first = new IntNode(x, first, null);
+        size += 1;
+        if (first.next != null) {
+            first.next.prev = first;
+        }
+        if (last == null) {
+            last = first;
+        }
     }
 
     public void addLast(T x) {
-	    last = new IntNode(x, null, last);
-	    size += 1;
-	    if (last.prev != null) {
-		    last.prev.next = last;
-	    }
-	    if (first == null) {
-	        first = last;
-	    }
+        last = new IntNode(x, null, last);
+        size += 1;
+        if (last.prev != null) {
+            last.prev.next = last;
+        }
+        if (first == null) {
+            first = last;
+        }
     }
 
     public boolean isEmpty() {
-	    return (first == null && last == null);
+        return (first == null && last == null);
     }
 
     public int size() {
-	    return size;
+        return size;
     }
 
     public void printDeque() {
-	    if (this.isEmpty()) {
-	        System.out.println("");
-	    } else {
-		    IntNode p = first;
-		    while (p != last) {
-			    System.out.print(p.item + " ");
-			    p = p.next;
-		    }
-		    System.out.print(p.item);
-		    System.out.println("");
-	    }
+        if (this.isEmpty()) {
+            System.out.println("");
+        } else {
+            IntNode p = first;
+            while (p != last) {
+                System.out.print(p.item + " ");
+                p = p.next;
+            }
+            System.out.print(p.item);
+            System.out.println("");
+        }
     }
 
     public T removeFirst() {
-	    if (this.isEmpty()) {
-		    return null;
-	    } else {
-		    size -= 1;
-		    T result = first.item;
-		    first = first.next;
-		    if (first == null) {  //empty now!
-			    last = null;
-			    return result;
-		    }
-		    first.prev = null;
-		    return result;
-	    }
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            size -= 1;
+            T result = first.item;
+            first = first.next;
+            if (first == null) {  //empty now!
+                last = null;
+                return result;
+            }
+            first.prev = null;
+            return result;
+        }
     }
 
     public T removeLast() {
-	    if (this.isEmpty()) {
-		    return null;
-	    } else {
-		    size -= 1;
-		    T result = last.item;
-		    last = last.prev;
-		    if (last == null) {  // empty now!
-			    first = null;
-			    return result;
-		    }
-		    last.next = null;
-		    return result;
-	    }
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            size -= 1;
+            T result = last.item;
+            last = last.prev;
+            if (last == null) {  // empty now!
+                first = null;
+                return result;
+            }
+            last.next = null;
+            return result;
+        }
     }
 
     public T get(int index) {
-	    int i = index;
-	    IntNode p = first;
-	    while (i != 0) {
-		    p = p.next;
-		    i--;
-	    }
-	    return p.item;
+        int i = index;
+        IntNode p = first;
+        while (i != 0) {
+            p = p.next;
+            i--;
+        }
+        return p.item;
     }
 
-    /** helper function. helps to get a new LinkedList removed first */
+    /**
+     * helper function. helps to get a new LinkedList removed first
+     */
     private LinkedListDeque getremove() {
-	    LinkedListDeque b = this.copy();
-	    b.removeFirst();
-	    return b;
+        LinkedListDeque b = this.copy();
+        b.removeFirst();
+        return b;
     }
 
     public T getRecursive(int index) {
-	    if (index == 0) {
-	        return first.item;
-	    } else {
-		    return (T) this.getremove().getRecursive(index - 1);
-	    }
+        if (index == 0) {
+            return first.item;
+        } else {
+            return (T) this.getremove().getRecursive(index - 1);
+        }
     }
 }
