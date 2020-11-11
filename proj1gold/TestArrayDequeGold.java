36b68922e298ca104798b41bd44bde2c44c55ee5
yogurt-shadow <1711143@mail.nankai.edu.cn>
2020-11-11 14:47:16 +0800

    proj1gold completed

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold{
+
+	@Test
+	public void testremove(){
+	StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+	ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
+	for(int i = 0; i < 10; i++){
+		double number = StdRandom.uniform();
+
+		if(number < 0.5){ sad1.addFirst(i); sad2.addFirst(i);}
+		else{ sad1.addLast(i);sad2.addLast(i);}
+	}
+
+	for(int i = 0; i < 10; i++){
+		double number = StdRandom.uniform();
+		if(number < 0.5){
+			int remove1 = sad1.removeFirst();
+			int remove2 = sad2.removeFirst();
+			assertEquals("This is bad!\nremoveFirst():", remove1, remove2);
+		}
+		else{
+			int remove1 = sad1.removeLast();
+			int remove2 = sad2.removeLast();
+			assertEquals("This is bad!\nremoveLast():", remove1, remove2);
+		}
+	}
+}
+
+@Test
+	public void testsize(){
+	StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+	ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
+	for(int i = 0; i < 10; i++){
+		double number = StdRandom.uniform();
+
+		if(number < 0.5){ sad1.addFirst(i); sad2.addFirst(i);}
+		else{ sad1.addLast(i);sad2.addLast(i);}
+	}
+
+	int size1 = sad1.size();
+	int size2 = sad2.size();
+	assertEquals("This is bad!\nsize():", size1, size2);
+}
+
+@Test
+	public void testget(){
+	StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+	ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
+	for(int i = 0; i < 10; i++){
+		double number = StdRandom.uniform();
+
+		if(number < 0.5){ sad1.addFirst(i); sad2.addFirst(i);}
+		else{ sad1.addLast(i);sad2.addLast(i);}
+	}
+
+	for(int i = 0; i < 10; i++){
+		Integer size1 =  sad1.get(i);
+		Integer size2 = sad2.getRecursive(i);
+		assertEquals("This is bad!\nsize():",size1, size2);
+	}
+}
+
+
+}
+
