07a6692846f5fbbcba47573a9ea4123ea10e3cd0
yogurt-shadow <1711143@mail.nankai.edu.cn>
2020-11-24 17:07:13 +0800

    add a new test

--- lab6_challenge/TestBubbleGrid.java
+++ lab6_challenge/TestBubbleGrid.java
@@ -54,4 +54,20 @@ public class TestBubbleGrid {
 		assertArrayEquals(pop1, new int[]{0, 0});
 	}
 
+	 @Test
+    public void testBasic() {
+
+        int[][] grid = {{1, 0, 0, 0},
+                        {1, 1, 1, 0}};
+        int[][] darts = {{1, 0}};
+        int[] expected = {2};
+
+        validate(grid, darts, expected);
+    }
+
+    private void validate(int[][] grid, int[][] darts, int[] expected) {
+        BubbleGrid sol = new BubbleGrid(grid);
+        assertArrayEquals(expected, sol.popBubbles(darts));
+    }
+
 }
