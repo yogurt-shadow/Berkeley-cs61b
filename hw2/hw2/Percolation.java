a1f6ab65418ba14776f50ae4baba277582a5ba56
yogurt-shadow <1711143@mail.nankai.edu.cn>
2020-11-25 13:47:24 +0800

    hw2 updated. Debug backwash

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -50,73 +50,21 @@ public class Percolation {
         if (isOpen(row, col)) {
             return;
         }
-        if (N == 1) {
-            grid[0][0] = 0;
-            sets.union(twoD_index(0, 0), N * N);
-            sets.union(twoD_index(0, 0), N * N + 1);
-            open_size += 1;
-            return;
-        }
         open_size += 1;
         grid[row][col] = 0; // open that site
         /**  the top site */
         if (row == 0) {
             sets.union(N * N, twoD_index(row, col));
             sets2.union(N * N, twoD_index(row, col));
-            if (isOpen(1, col)) {
-                sets.union(twoD_index(1, col), twoD_index(row, col));
-                sets2.union(twoD_index(1, col), twoD_index(row, col));
-            }
-            return;
         }
         /**  the bottom line */
         if (row == N - 1) {
             sets.union(N * N + 1, twoD_index(row, col));
-            if (isOpen(N - 2, col)) {
-                sets.union(twoD_index(N - 2, col), twoD_index(row, col));
-                sets2.union(twoD_index(N - 2, col), twoD_index(row, col));
-            }
-            if (col - 1 >= 0 && isOpen(row, col - 1)) {
-                sets.union(twoD_index(row, col - 1), twoD_index(row, col));
-                sets2.union(twoD_index(row, col - 1), twoD_index(row, col));
-            }
-
-            if (col + 1 < N && isOpen(row, col + 1)) {
-                sets.union(twoD_index(row, col + 1), twoD_index(row, col));
-                sets2.union(twoD_index(row, col + 1), twoD_index(row, col));
-            }
-            return;
         }
-
-        /** the left line */
-        if (col == 0) {
-            for (int i = 1; i < 4; i++) {
-                if (isOpen(row + dir1[i], col + dir2[i])) {
-                    sets.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                    sets2.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                }
-            }
-            return;
-        }
-
-        /**  the right line */
-        if (col == N - 1) {
-            for (int i = 0; i < 3; i++) {
-                if (isOpen(row + dir1[i], col + dir2[i])) {
-                    sets.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                    sets2.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                }
-            }
-            return;
-        }
-
-        /** the other site */
-        else {
-            for (int i = 0; i < 4; i++) {
-                if (isOpen(row + dir1[i], col + dir2[i])) {
-                    sets.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                    sets2.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
-                }
+        for (int i = 0; i < 4; i++) {
+            if (!outside(row + dir1[i]) && !outside(col + dir2[i]) && isOpen(row + dir1[i], col + dir2[i])) {
+                sets.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
+                sets2.union(twoD_index(row, col), twoD_index(row + dir1[i], col + dir2[i]));
             }
         }
 
@@ -144,32 +92,33 @@ public class Percolation {
         return sets.connected(N * N, N * N + 1);
     }
 
+/**
+ public static void main(String[] args) {
+ */
+    /**
+     p1.open(2, 2);
+     p1.open(2, 3);
+     System.out.println(p1.isOpen(2, 2));
+     System.out.println(p1.isFull(2, 2));
+     p1.open(0, 2);
+     System.out.println(p1.isOpen(0, 2));
+     p1.open(0, 3);
+     System.out.println(p1.percolates());
+     System.out.println(p1.numberOfOpenSites());
+     p1.open(3, 2);
+     p1.open(1, 3);
+     System.out.println(p1.isFull(2, 2));
+     System.out.println(p1.percolates());
 
-    public static void main(String[] args) {
-        /**
-         p1.open(2, 2);
-         p1.open(2, 3);
-         System.out.println(p1.isOpen(2, 2));
-         System.out.println(p1.isFull(2, 2));
-         p1.open(0, 2);
-         System.out.println(p1.isOpen(0, 2));
-         p1.open(0, 3);
-         System.out.println(p1.percolates());
-         System.out.println(p1.numberOfOpenSites());
-         p1.open(3, 2);
-         p1.open(1, 3);
-         System.out.println(p1.isFull(2, 2));
-         System.out.println(p1.percolates());
+     System.out.println();
 
-         System.out.println();
+     Percolation p2 = new Percolation(3);
+     p2.open(1, 1);
+     p2.open(2, 1);
+     System.out.println(p2.percolates());
+     p2.open(0, 1);
+     System.out.println(p2.percolates());
+     */
 
-         Percolation p2 = new Percolation(3);
-         p2.open(1, 1);
-         p2.open(2, 1);
-         System.out.println(p2.percolates());
-         p2.open(0, 1);
-         System.out.println(p2.percolates());
-         */
-    }
-
+    /**  }*/
 }
